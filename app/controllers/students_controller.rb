93db41c1ff05fcb7c306ee2b2add603a7307fbaa
Kyle <khartson2017@gmail.com>
2022-07-27 12:09:58 -0400

    completed assignment

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,33 @@
+class StudentsController < ApplicationController
+ 
+  def index
+    render json: Student.all
+  end 
+
+  def show
+    student = find_student
+    render json: student
+  end 
+
+  def create
+    student = Student.create!(student_params)
+    render json: student, status: :created
+  end 
+
+  def update 
+    student = find_student
+    student.update!(student_params)
+    render json: student
+  end 
+
+  private 
+
+  def find_student
+    Student.find(params[:id])
+  end 
+
+  def student_params
+    params.permit(:name, :major, :age, :instructor_id)
+  end 
+
+end
